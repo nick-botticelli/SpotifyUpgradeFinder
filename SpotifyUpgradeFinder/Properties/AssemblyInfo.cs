003bffc8d0309a3f07d4e0f01ca9f26882af6a6a
nick-botticelli <nick.s.botticelli@gmail.com>
2020-05-19 02:22:10 -0700

    Update repo with final initial changes

--- SpotifyUpgradeFinder/Properties/AssemblyInfo.cs
+++ SpotifyUpgradeFinder/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SpotifyUpgradeFinder")]
-[assembly: AssemblyCopyright("Copyright © Nick Botticelli 2020")]
+[assembly: AssemblyCopyright("Copyright © Nicholas Botticelli 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
